14756b53daf4d52f515709f467caf6412ff1d6ba
PabloSaura <pablosaauraa@gmail.com>
2023-05-12 14:11:04 +0200

    MENÚS VARIOS

---
+++ Assets/Codigos/StartScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EntrarJuego(){
+        SceneManager.LoadScene("Main_Nivel");
+        
+    }
+    public void EleccionPersonajes(){
+    SceneManager.LoadScene("Eleccion_Personajes");
+    }
+
+    public void Tutorial(){
+    SceneManager.LoadScene("Tutorial");
+    }
+}
