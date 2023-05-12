1665390dd4d0bbeb64ce1b3a50ec02e3574b31ae
Dadoso22 <dadoso22@hotmail.com>
2023-05-12 14:21:54 +0200

    12/05/23
    Optimización de Ficheros,
    Implementando Inputs de Mando,

---
+++ Assets/Codigos/inputArco.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class inputArco : MonoBehaviour
+{
+    InputMaster controles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        
+    }
+
+    void Awake () {
+        controles = new InputMaster();
+
+    
+    }
+}
