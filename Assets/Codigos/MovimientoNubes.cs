63c4430e5074de603d70aa6468abd1bb3316f381
PabloSaura <pablosaauraa@gmail.com>
2023-05-17 14:38:31 +0200

    17/05 hay que corregir el movimiento de las nubes

--- Assets/Codigos/MovimientoNubes.cs
+++ Assets/Codigos/MovimientoNubes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovimientoNubes : MonoBehaviour
 {
-    //public static nubes;
+    //public float nubes;
     //public float velocidadNubes = 10f;
 
     // Start is called before the first frame update
