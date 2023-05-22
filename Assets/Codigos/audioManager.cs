94ee0eb2aa992cd1403e233b1bc2a10efac34c24
Damián Domínguez <dadoso22@hotmail.comgit config --global user.name Dadoso22>
2023-05-23 01:23:35 +0200

    Catapulta y Banda Sonora, mediano éxito

--- Assets/Codigos/audioManager.cs
+++ Assets/Codigos/audioManager.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class audioManager : MonoBehaviour
 {
 
-    public AudioClip sonidoBeso;
+    public AudioClip bandaSonora1;
+    public AudioClip clicMenu;
     public AudioClip sonidoDisparo;
     public AudioClip sonidoImpacto;
+    public AudioClip sonidoBeso;
+
+
+    private AudioSource hiloMusical;
+
+
+    public GameObject scriptDuplicado;
+    void Awake () {
+        DontDestroyOnLoad(this.gameObject);
+
+        if(scriptDuplicado == null) {
+           scriptDuplicado = this.gameObject; 
+        } else if (scriptDuplicado != null) {
+            DestroyObject(this.gameObject);
+        }
+    } //Fin Awake
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hiloMusical = GetComponent<AudioSource>();  //
+        hiloMusical.clip = bandaSonora1;
+        hiloMusical.loop = true;
+        hiloMusical.Play();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -22,9 +43,11 @@ public class audioManager : MonoBehaviour
         
     }
 
+
 /*
     public void sonidoBesito () {
         hiloMusical.PlayOneShot(sonidoBesito, 1f);
     }
 */
+
 }
