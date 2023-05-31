9ca211e00b02eecf8b6ef511a50ece889f180a4b
PatLoPa <patricialo99@hotmail.com>
2023-05-31 13:23:54 +0200

    Texto en banderines

--- Assets/Codigos/MainScript.cs
+++ Assets/Codigos/MainScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class MainScript : MonoBehaviour
 
@@ -20,8 +20,29 @@ public class MainScript : MonoBehaviour
     //Vida Capsules
     public static int Max_Vida_Capsules = 10; // lo he cambiado de 5 --> 3
 
-    public int Vida_Capsule_Izquierda, Puntos_Izquierda;
-    public int Vida_Capsule_Derecha, Puntos_Derecha;
+    public int Vida_Capsule_Izquierda;
+    private int _Puntos_Izquierda;
+    public int Puntos_Izquierda
+    {
+        get => _Puntos_Izquierda;
+        set
+        {
+            _Puntos_Izquierda = value;
+            TextoPuntuacionIzquierda.text = value.ToString();
+        }
+    }
+    public int Vida_Capsule_Derecha;
+    private int _Puntos_Derecha;
+    public int Puntos_Derecha
+    {
+        get => _Puntos_Derecha;
+        set
+        {
+            _Puntos_Derecha = value;
+            TextoPuntuacionDerecha.text = value.ToString();
+        }
+    }
+
 
     ///mi Skin
     public GUISkin miSkin;
@@ -54,20 +75,20 @@ public class MainScript : MonoBehaviour
         Besito = false;
     }
 
-    void OnGUI(){
-        GUI.skin = miSkin;
-        GUI.Label(new Rect(870,40,150,80),"RONDA: "+Ronda.ToString(),"estiloJuego");
-        //GUI.Label(new Rect(20,20,150,80),"Vida: "+vidas.ToString(),"estiloScore");
-        //GUI.Label(new Rect(100,20,150,80),"Score: "+score.ToString(),"estiloScore");
-        GUI.Label(new Rect(510,20,150,80),""+Puntos_Izquierda.ToString(),"estiloJuego");
-        GUI.Label(new Rect(1369,20,150,80),""+Puntos_Derecha.ToString(),"estiloJuego");
-        //GUI.Label(new Rect(180,20,150,80),"Enemigos: "+enemigos.ToString(),"estiloScore");
-        //GUI.Label(new Rect(Anchopantalla-100,20,200,100),"Nombre Juego", "estiloTitulo"); /// el primero sería (380,x,x,x)
-        ///imagen
-        //GUI.DrawTexture(new Rect(Screen.width-200,20,80,80),logoJuego);
+    //void OnGUI(){
+    //    GUI.skin = miSkin;
+    //    GUI.Label(new Rect(870,40,150,80),"RONDA: "+Ronda.ToString(),"estiloJuego");
+    //    //GUI.Label(new Rect(20,20,150,80),"Vida: "+vidas.ToString(),"estiloScore");
+    //    //GUI.Label(new Rect(100,20,150,80),"Score: "+score.ToString(),"estiloScore");
+    //    GUI.Label(new Rect(510,20,150,80),""+Puntos_Izquierda.ToString(),"estiloJuego");
+    //    GUI.Label(new Rect(1369,20,150,80),""+Puntos_Derecha.ToString(),"estiloJuego");
+    //    //GUI.Label(new Rect(180,20,150,80),"Enemigos: "+enemigos.ToString(),"estiloScore");
+    //    //GUI.Label(new Rect(Anchopantalla-100,20,200,100),"Nombre Juego", "estiloTitulo"); /// el primero sería (380,x,x,x)
+    //    ///imagen
+    //    //GUI.DrawTexture(new Rect(Screen.width-200,20,80,80),logoJuego);
 
 
-    }
+    //}
     /*
         public void IniciaPosicion1(){
             cap1.transform.position = new Vector2(21,-6.1f); 
