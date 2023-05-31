ccf60c9517a23dcf80bbf9a7fd2dddb9f4f0120c
PatLoPa <patricialo99@hotmail.com>
2023-05-31 12:54:09 +0200

    Arreglada la serialización de los personajes seleccionados

--- Assets/ScriptableObjects/PersonajesSeleccionados.cs
+++ Assets/ScriptableObjects/PersonajesSeleccionados.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PersonajesSeleccionados", menuName = "CATAPULETOS_juego/PersonajesSeleccionados", order = 0)]
 public class PersonajesSeleccionados : ScriptableObject
 {
+    private void OnEnable()
+    {
+        hideFlags = HideFlags.DontUnloadUnusedAsset;
+    }
     public int IndicePersonajeIzquierda = 0, IndicePersonajeDerecha = 5;
     public GameObject[] personajes;
     public GameObject PersonajeIzquierda { get => personajes[IndicePersonajeIzquierda]; }
